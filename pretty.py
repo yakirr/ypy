bd640cbc3a3d6a64d1cafddaa31cdafac4c2c327
Yakir Abraham Reshef <yar2@loge.orchestra>
2016-04-07 09:39:30 -0400

    name_width bug fix

--- pretty.py
+++ pretty.py
@@ -3,4 +3,4 @@ from __future__ import print_function, division
 def print_namespace(ns, name_width=25):
     for name, val in sorted(vars(ns).items()):
         if not name.startswith('_'):
-            print('{:<'+str(name_width)+'}{}'.format(name, val))
+            print(('{:<'+str(name_width)+'}{}').format(name, val))
