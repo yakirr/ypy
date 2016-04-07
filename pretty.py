8005b3ff2c03f36804e82d4468a522225c4a5f1e
Yakir Abraham Reshef <yar2@loge.orchestra>
2016-04-07 09:38:06 -0400

    added name_width

--- pretty.py
+++ pretty.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, division
 
-def print_namespace(ns):
+def print_namespace(ns, name_width=25):
     for name, val in sorted(vars(ns).items()):
         if not name.startswith('_'):
-            print('{:<25}{}'.format(name, val))
+            print('{:<'+str(name_width)+'}{}'.format(name, val))
