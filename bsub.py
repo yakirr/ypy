542f84e87a811e4145739107694f937b8e9b4c8d
Yakir Abraham Reshef <yar2@loge.orchestra>
2016-05-20 10:00:26 -0400

    job dependencies, make dir for file

--- bsub.py
+++ bsub.py
@@ -3,19 +3,23 @@ import subprocess
 import sys
 import pretty
 
-def add_main_and_submit(parser, main_function, submit_function, merge_function=None):
+def add_main_and_submit(parser, main_function, submit_function,
+        merge_function=None,
+        main_parents=[],
+        submit_parents=[],
+        merge_parents=[]):
     subparsers = parser.add_subparsers()
 
-    subparser_main = subparsers.add_parser('main')
+    subparser_main = subparsers.add_parser('main', parents=main_parents)
     subparser_main.set_defaults(_func=main_function)
 
-    subparser_submit = subparsers.add_parser('submit')
+    subparser_submit = subparsers.add_parser('submit', parents=submit_parents)
     subparser_submit.set_defaults(_func=submit_function)
 
     if merge_function is None:
         return subparser_main, subparser_submit
     else:
-        subparser_merge = subparsers.add_parser('merge')
+        subparser_merge = subparsers.add_parser('merge', parents=merge_parents)
         subparser_merge.set_defaults(_func=merge_function)
         return subparser_main, subparser_submit, subparser_merge
 
@@ -33,7 +37,7 @@ def param_dict_to_commandline_list(d):
     return result
 
 def bsub_command(command, outfilepath, jobname=None, queue='short', time_in_hours=12,
-        time_in_minutes=None, memory_GB=8):
+        time_in_minutes=None, memory_GB=8, depends_on=None):
     if time_in_minutes:
         time = '0:' + str(time_in_minutes)
     else:
@@ -43,14 +47,20 @@ def bsub_command(command, outfilepath, jobname=None, queue='short', time_in_hour
         ['-q', queue,
         '-W', time,
         '-oo', outfilepath,
-        '-R', '"rusage[mem=' + str(int(memory_GB * 1024)) + '] select[model!=XeonE5345 && model!=XeonE5430]"',
-        ' '.join(command)]
+        '-R', '"rusage[mem=' + str(int(memory_GB * 1024)) + \
+                '] select[model!=XeonE5345 && model!=XeonE5430]"'] + \
+            (['-w', 'done({})'.format(depends_on)] if depends_on is not None else []) + \
+        [' '.join(command)]
 
 def submit(command, outfilepath, jobname=None, queue='short', time_in_hours=12,
-        time_in_minutes=None, memory_GB=8, debug=False):
+        time_in_minutes=None, memory_GB=8, depends_on=None, debug=False):
     bsub_cmd = bsub_command(command, outfilepath, jobname, queue,
-            time_in_hours, time_in_minutes, memory_GB)
+            time_in_hours, time_in_minutes, memory_GB, depends_on)
     print('\033[92m' + ' '.join(bsub_cmd) + '\033[0m')
     print('\033[94m' + outfilepath + '\033[0m')
     if not debug:
-        subprocess.call(bsub_cmd)
+        output = subprocess.check_output(bsub_cmd)
+        print(output)
+        return output.replace('[','<').replace('>','<').split('<')[1] # parse out job id
+    else:
+        return 'JOBID'
--- fs.py
+++ fs.py
@@ -2,6 +2,9 @@ from __future__ import print_function, division
 import os
 
 # Create directory if necessary. If the directory exists do nothing
-def makedir(directory_name):
-    if not os.path.exists(directory_name):
-        os.makedirs(directory_name)
+def makedir(path_to_dir):
+    if not os.path.exists(path_to_dir):
+        os.makedirs(path_to_dir)
+
+def makedir_for_file(path_to_file):
+    makedir(os.path.dirname(path_to_file))
