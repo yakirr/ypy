5967e5a4c01e36712633c64714bc6bbbfc16d322
Yakir Abraham Reshef <yar2@loge.orchestra>
2017-10-30 17:29:54 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='ypy',
     version='0.1',
     description='Python utility functions used in yakirr repos',
-    url='http://github.com/yarkirr/ypy',
+    url='http://github.com/yakirr/ypy',
     author='Yakir Reshef',
     author_email='yakir@seas.harvard.edu',
     license='MIT',
