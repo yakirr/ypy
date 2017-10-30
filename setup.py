46a92eb0efe664ce022258d0b79cfbb1a5ff90c7
Yakir Abraham Reshef <yar2@loge.orchestra>
2017-10-30 16:42:50 -0400

    added setuptools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ypy',
+    version='0.1',
+    description='Python utility functions used in yakirr repos',
+    url='http://github.com/yarkirr/ypy',
+    author='Yakir Reshef',
+    author_email='yakir@seas.harvard.edu',
+    license='MIT',
+    packages=['ypy'],
+    zip_safe=False)
